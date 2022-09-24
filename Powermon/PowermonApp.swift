20104d2a90cca6bd8673ca86b3be6737e267c763
vinitshandilya <vinitshandilya@outlook.com>
2022-09-24 18:08:47 +0530

    Initial commit

---
+++ Powermon/PowermonApp.swift
@@ -0,0 +1,17 @@
+//
+//  PowermonApp.swift
+//  Powermon
+//
+//  Created by vshandil on 24/09/22.
+//
+
+import SwiftUI
+
+@main
+struct PowermonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
